f7ffa2152ccc19e1c1b550864581fe443bf3936c
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-02-02 10:56:36 -0500

    add starter code for 03-OO-ruby-pt2

---
+++ 03-OO-ruby-pt2/lib/Patient.rb
@@ -0,0 +1,33 @@
+class Patient
+    attr_accessor :name, :age, :owner, :number
+    attr_reader :species
+    @@all = []
+    def initialize(name, species="cat", age, owner,number)
+        @species = species
+        @name = name
+        @age = age
+        @owner = owner
+        @number = number
+        @@all << self
+    end 
+#READ
+#Lets us call Patient.all, a class method
+    def self.all
+        @@all
+    end 
+    
+#Lets us call instance.print_info a instance method
+    def print_info
+        puts "Name: #{@name}"
+        puts "Species: #{@species}"
+        puts "Age: #{@age}"
+        puts "Owner: #{@owner}"
+        puts "Phone number: #{@number}"
+        puts "--------------------------------"
+    end 
+
+   
+
+   
+
+end 
